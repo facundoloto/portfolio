3259d839b11d82fc9014c4ceaa2007cce7037815
ema <lotofacundo6@gmail.com>
2024-02-15 10:50:20 -0300

    fix err video url

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,14 +1,16 @@
 import ReactPlayer from 'react-player';
 import { useTranslation } from "react-i18next";
+import es from './../../assets/video/me_es.mp4';
+import eng from './../../assets/video/me_eng.mp4';
 
 const VideoPlayer = () => {
-    const videoUrl = './src/assets/video/me_';
+    // const videoUrl = './src/assets/video/me_';
     const { t, i18n } = useTranslation("about");
 
     return (
         <div>
             <ReactPlayer
-                url={videoUrl + t("translations.video") + ".mp4"}
+                url={t("translations.video") == "eng" ? eng : es}
                 width="100%"
                 height="100%"
                 controls
